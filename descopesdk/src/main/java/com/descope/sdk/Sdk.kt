2d521ab7001d754ba75805f7a7e9225dc40c7a6c
itaihanski <itaihanski@gmail.com>
2024-01-24 13:14:25 +0200

    Bump version to 0.9.7

--- descopesdk/src/main/java/com/descope/sdk/Sdk.kt
+++ descopesdk/src/main/java/com/descope/sdk/Sdk.kt
@@ -71,6 +71,6 @@ class DescopeSdk(val config: DescopeConfig) {
         const val name = "DescopeAndroid"
 
         /** The Descope SDK version */
-        const val version = "0.9.6"
+        const val version = "0.9.7"
     }
 }
