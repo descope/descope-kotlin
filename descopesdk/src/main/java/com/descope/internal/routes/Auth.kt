dcb149ae2975a1d0b5c563449d1c71313c27e2ac
Itai Hanski <itaihanski@gmail.com>
2025-04-08 15:23:06 +0300

    Add the ability to exchange an external token with a Descope session (#200)

--- descopesdk/src/main/java/com/descope/internal/routes/Auth.kt
+++ descopesdk/src/main/java/com/descope/internal/routes/Auth.kt
@@ -2,7 +2,10 @@ package com.descope.internal.routes
 
 import com.descope.internal.http.DescopeClient
 import com.descope.internal.http.TenantsResponse
+import com.descope.internal.others.with
 import com.descope.sdk.DescopeAuth
+import com.descope.types.AuthenticationResponse
+import com.descope.types.DescopeException
 import com.descope.types.DescopeTenant
 import com.descope.types.DescopeUser
 import com.descope.types.RevokeType
@@ -18,6 +21,20 @@ internal class Auth(private val client: DescopeClient) : DescopeAuth {
 
     override suspend fun refreshSession(refreshJwt: String): RefreshResponse =
         client.refresh(refreshJwt).toRefreshResponse()
+
+    override suspend fun migrateSession(externalToken: String): AuthenticationResponse {
+        val refreshResponse = client.exchangeExternalToken(externalToken).toRefreshResponse()
+        if (refreshResponse.refreshToken == null) {
+            throw DescopeException.decodeError.with(message = "External token exchange did not return a refresh token")
+        }
+        val user = me(refreshResponse.refreshToken.jwt)
+        return AuthenticationResponse(
+            sessionToken = refreshResponse.sessionToken,
+            refreshToken = refreshResponse.refreshToken,
+            user = user,
+            isFirstAuthentication = false,
+        )
+    }
 
     override suspend fun revokeSessions(revokeType: RevokeType, refreshJwt: String) {
         client.logout(refreshJwt, revokeType)
