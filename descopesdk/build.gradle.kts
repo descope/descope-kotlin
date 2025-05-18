7f572dc4eacb400292693a6133432502d53661d8
Itai Hanski <itaihanski@gmail.com>
2025-05-18 12:33:54 +0300

    Migrate gradle files to Kotlin DSL (#219)

---
+++ descopesdk/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish")
+    id("signing")
+}
+
+android {
+    namespace = "com.descope"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 35
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.common)
+    implementation(libs.lifecycle.process)
+    implementation(libs.browser)
+    implementation(libs.androidx.security.crypto)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.json)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.descope"
+            artifactId = "descope-kotlin"
+            version = System.getenv("DESCOPESDK_VERSION")
+            pom {
+                name = project.name
+                description = project.name
+                url = "https://github.com/descope/descope-kotlin"
+                inceptionYear = "2023"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "http://www.opensource.org/licenses/mit-license.php"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "descope"
+                        name = "Descope Inc"
+                    }
+                }
+                scm {
+                    connection = "scm:https://github.com/descope/descope-kotlin.git"
+                    developerConnection = "scm:git@github.com:descope/descope-kotlin.git"
+                    url = "https://github.com/descope/descope-kotlin"
+                }
+            }
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = System.getenv("PGP_KEY")
+    val signingPassword = System.getenv("PGP_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["release"]) 
+}
