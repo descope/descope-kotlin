48a065a234692dd554a7fd62019b3cdac9fe11b9
Itai Hanski <itaihanski@gmail.com>
2026-04-29 14:20:32 +0300

    Bump version

--- descopesdk/src/main/java/com/descope/sdk/Sdk.kt
+++ descopesdk/src/main/java/com/descope/sdk/Sdk.kt
@@ -79,6 +79,6 @@ class DescopeSdk(context: Context, projectId: String, configure: DescopeConfig.(
         const val NAME = "DescopeAndroid"
 
         /** The Descope SDK version */
-        const val VERSION = "0.18.1"
+        const val VERSION = "0.18.2"
     }
 }
